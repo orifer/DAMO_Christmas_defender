9027fd35160b82a617bd119fb5720ed6e5b385ff
orifer <oriol.fernandez@hotmail.com>
2021-11-28 16:57:52 +0100

    Ball refactor and cleanup

--- app/src/main/java/edu/upc/epsevg/damo/a08_christmas_defender/Ball.java
+++ app/src/main/java/edu/upc/epsevg/damo/a08_christmas_defender/Ball.java
@@ -1,7 +1,5 @@
 package edu.upc.epsevg.damo.a08_christmas_defender;
 
-import android.util.Log;
-
 import java.util.ArrayList;
 
 public class Ball {
@@ -42,29 +40,6 @@ public class Ball {
         c = point.sum(p, point.mul(r, point.unitary(pc)));
     }
 
-//    public void pushAside(Segment segment) {
-//        point p1 = segment.p1;
-//        point p2 = segment.p2;
-//        pushAside(p1);
-//        pushAside(p2);
-//        point p1p2 = point.sub(p2,p1);
-//        point p1c = point.sub(c, p1);
-//        double scalarProd = point.scalarProd(p1p2, p1c);
-//
-//        // Hacer que no sean segmentos extendidos hasta infinito
-//        if (scalarProd <= 0) return;
-//        if (scalarProd >= point.norm(p1p2)) return;
-//        point projection = point.sum(p1, point.mul(scalarProd/point.norm(p1p2),p1p2));
-//        pushAside(projection);
-//    }
-
-//    public void pushAside(SegmentsManager segmentsManager) {
-//        ArrayList<Segment> list = segmentsManager.list;
-//        for (Segment s : list) {
-//            pushAside(s);
-//        }
-//    }
-
     public boolean pushAside(Polygonal polygon) {
         point p1 = polygon.center;
         point p2 = point.mul(0.9,polygon.center); // Para controlar hitbox
@@ -100,16 +75,6 @@ public class Ball {
         if (p != null)
             polygonsManager.list.remove(p);
     }
-
-//    public void move(double delta, SegmentsManager segmentsManager) {
-//        // Evitar wall tunneling. Nos aseguramos de que no nos movemos mas de r/2
-//        double traversed = r * speedFactor * delta;
-//        int steps = (int) (traversed / (r/2.0)) + 1;
-//        for (int i = 0; i < steps; i++) {
-//            move(delta/steps);
-//            pushAside(segmentsManager);
-//        }
-//    }
 
     public void move(double delta, PolygonalManager polygonalManager) {
         // Evitar wall tunneling. Nos aseguramos de que no nos movemos mas de r/2
